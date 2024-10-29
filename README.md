1e315fd44a4b6b8244de704f137b2cc93aa40a54
Keerthana-Shri <ekeerthanashri@gmail.com>
2024-10-29 20:10:22 +0530

    FastAPI-Pydantic task2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Practice_Python_with_Udemy
+# Branch : Pydantic
+- Pydantic Book Request Validation
+- Post Request
+- Data Validation Path Parameters
+- Data Validation Query Parameters
+- Fetch Book by Rating
+- Update Book with PUT Request
+- Delete book with DELETE Request
+- HTTP Exception
---
+++ books2.py
@@ -0,0 +1,120 @@
+from typing import Optional
+from fastapi import FastAPI, Path, Query, HTTPException, Body
+from pydantic import BaseModel, Field
+from starlette import status
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+    def __init__(self, id, title, author, description, rating, published_date):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description='ID is not needed on create', default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "The Great Adventure",
+                "author": "John Smith",
+                "description": "Admire nature",
+                "rating": 5,
+                'published_date': 2021
+            }
+        }
+    }
+
+
+BOOKS = [
+    Book(1, 'Cooking Made Simple', 'Emma Brown', 'Cooking Recipes', 4, 2019),
+    Book(2, 'Famous Five', 'Enid Blyton', 'Fantasy and adventures', 5, 1966),
+    Book(3, 'The Alchemist', 'Paulo Coaho', 'Fictional motivation', 4, 1988),
+    Book(4, 'The Art of Mindfulness', 'Sophia Wilson', 'Practical advice for mindfulness in mind', 3, 2018),
+    Book(5, 'Half Girlfriend', 'Chetan Bhagat', 'Romance and suspense', 4, 2017),
+    Book(6, 'Your the Best Wife', 'Ajay K Pandey', 'Biography of author', 5, 2015)
+]
+
+
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail='Item not found')
+
+
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
+
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
